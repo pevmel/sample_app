d5c7df8fe161d39b06b67ec14b9ab850daebf4f3
Viacheslav Panchuk <pevmel@gmail.com>
2018-07-01 17:35:30 +0300

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,17 @@ Rails.application.routes.draw do
   get 'contact', to: 'static_pages#contact'
   get 'signup', to: 'users#new'
   post 'signup',  to: 'users#create'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get    'login',   to: 'sessions#new'
   post   'login',   to: 'sessions#create'
   delete 'logout',  to: 'sessions#destroy'
   resources :account_activations, only: :edit
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
